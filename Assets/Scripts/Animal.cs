a9a719b6809f5e7f1219162589c6b3262b22487a
Ferhed <JeSuisLeMeilleurProg@Programmateur.prog>
2017-01-20 23:50:21 +0100

    add animal spawner

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class Animal : MonoBehaviour
 {
     public AnimalInfo animalInfo;
+    public float speed = 10.0f;
+
+    private void Update()
+    {
+        if(IsDrifting)
+        {
+            transform.position = transform.position + Vector3.right * Time.deltaTime * speed;
+        }
+    }
+
+    public bool IsDrifting = true;
 }
---
+++ Assets/Scripts/AnimalSpawnerManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSpawnerManager : MonoBehaviour {
+
+    public static AnimalSpawnerManager instance;
+
+    public float offSetXToSpawn = -40.0f;
+    public float offSetZToSpawn = 15.0f;
+    public float offSetZNearlyShip = 2.0f;
+
+    public bool CanInvoke = true;
+
+    public float MinInvokeTime = 1.0f;
+    public float MaxInvokeTime = 3.0f;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        animalManager = AnimalManager.instance;
+
+        Invoke( "InvokeAPoorPuppy", 1.0f );
+    }
+
+    private void InvokeAPoorPuppy()
+    {
+        if(!CanInvoke)
+        {
+            return;
+        }
+
+        animalManager.GenerateAnimal( GetSpawnPosition() );
+
+        Invoke( "InvokeAPoorPuppy", Random.Range( MinInvokeTime, MaxInvokeTime ) );
+    }
+
+    private Vector3 GetSpawnPosition()
+    {
+        float ZValue = Random.value < 0.5f ? Random.Range( -offSetZToSpawn, -offSetZNearlyShip ) : Random.Range( offSetZToSpawn, offSetZNearlyShip );
+
+        return new Vector3( offSetXToSpawn, 0, ZValue );
+    }
+
+    private AnimalManager animalManager;
+}
